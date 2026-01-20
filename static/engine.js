d56c0d3fac030225abf5eb804a2dacf15e16d559
Reza-namvaran <reza.namvaran823@gmail.com>
2026-01-21 00:29:13 +0330

    (feat): Add canvas with basic shapes

---
+++ static/engine.js
@@ -0,0 +1,111 @@
+let svg = document.getElementById("canvas");
+let shapes = [];
+let dragging = null;
+let offsetX = 0;
+let offsetY = 0;
+
+function createSVG(tag) {
+    return document.createElementNS("http://www.w3.org/2000/svg", tag);
+}
+
+function addShape(type) {
+    let shape = {id: Date.now(), type, x: 100, y: 100, w: 120, h: 60, text: type.toUpperCase()};
+    shapes.push(shape);
+    render();
+}
+
+function addRect() {
+    addShape("rect");
+}
+
+function addDoubleRect() {
+    addShape("double-rect");
+}
+
+function addDiamond() {
+    addShape("diamond");
+}
+
+function startDrag(e) {
+    let id = Number(this.getAttribute("data-id"));
+    dragging = shapes.find(s => s.id === id);
+    offsetX = e.clientX - dragging.x;
+    offsetY = e.clientY - dragging.y;
+}
+
+function render() {
+    svg.innerHTML = "";
+
+    shapes.forEach(shape => {
+        let g = createSVG("g");
+        g.classList.add("shape");
+        g.setAttribute("data-id", shape.id);
+        g.addEventListener("mousedown", startDrag)
+    
+        if (shape.type === "rect") {
+            let r = createSVG("rect");
+            r.setAttribute("x", shape.x);
+            r.setAttribute("y", shape.y);
+            r.setAttribute("width", shape.w);
+            r.setAttribute("height", shape.h);
+            r.setAttribute("fill", "white");
+            r.setAttribute("stroke", "black");
+            g.appendChild(r);
+        }
+
+        if (shape.type == "double-rect") {
+            let innerRect = createSVG("rect");
+            let outerRect = createSVG("rect");
+
+            innerRect.setAttribute("x", shape.x);
+            innerRect.setAttribute("y", shape.y);
+            innerRect.setAttribute("width", shape.w);
+            innerRect.setAttribute("height", shape.h);
+
+            outerRect.setAttribute("x", shape.x + 5);
+            outerRect.setAttribute("y", shape.y + 5);
+            outerRect.setAttribute("width", shape.w - 10);
+            outerRect.setAttribute("height", shape.h - 10);
+
+            [innerRect, outerRect].forEach(Rect => {
+                Rect.setAttribute("fill", "white");
+                Rect.setAttribute("stroke", "black");
+                g.appendChild(Rect);
+            })
+        }
+
+        if (shape.type === "diamond") {
+            let p = createSVG("polygon");
+            let points = `
+              ${shape.x + shape.w/2},${shape.y}
+              ${shape.x + shape.w},${shape.y + shape.h/2}
+              ${shape.x + shape.w/2},${shape.y + shape.h}
+              ${shape.x},${shape.y + shape.h/2}
+            `;
+            p.setAttribute("points", points);
+            p.setAttribute("fill", "white");
+            p.setAttribute("stroke", "black");
+            g.appendChild(p);
+          }
+      
+          let t = createSVG("text");
+          t.setAttribute("x", shape.x + shape.w/2);
+          t.setAttribute("y", shape.y + shape.h/2);
+          t.setAttribute("text-anchor", "middle");
+          t.setAttribute("dominant-baseline", "middle");
+          t.textContent = shape.text;
+          g.appendChild(t);
+
+          svg.appendChild(g);
+    });
+}
+
+svg.addEventListener("mousemove", e => {
+    if (!dragging) return;
+    dragging.x = e.clientX - offsetX;
+    dragging.y = e.clientY - offsetY;
+    render();
+});
+
+svg.addEventListener("mouseup", () => dragging = null);
+svg.addEventListener("mouseleave", () => dragging = null);
